71b8aaa3f037c3a639fdc97c740fb4b96e9e6d4b
DONGAlex03 <lyzerdong03@gmail.com>
2023-12-27 23:05:49 +0100

    chore: test workflow

--- project/backend/dump.sql
+++ project/backend/dump.sql
@@ -1,14 +1,14 @@
 -- Create the table
-CREATE TABLE your_table_name (
+CREATE TABLE books (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(45) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    desc VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     cover VARCHAR(45) NOT NULL
 );
 
 -- Insert some sample data
-INSERT INTO your_table_name (title, description, price, cover) VALUES
+INSERT INTO books (title, desc, price, cover) VALUES
     ('Title 1', 'Description 1', 100, 'cover1.jpg'),
     ('Title 2', 'Description 2', 150, 'cover2.jpg'),
     ('Title 3', 'Description 3', 200, 'cover3.jpg');
