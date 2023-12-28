26ebf293b7672c43df01d5b5672b4820ef093020
DONGAlex03 <lyzerdong03@gmail.com>
2023-12-28 15:01:17 +0100

    chore: test workflow

--- project/backend/dump.sql
+++
@@ -1,9 +0,0 @@
--- Create the table
-CREATE TABLE books (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(45) NOT NULL,
-    desc VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
-    cover VARCHAR(45) NOT NULL
-);
-
