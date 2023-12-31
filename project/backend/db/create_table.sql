1c7e3a0d650fd54807e4eeb86537941a692240ed
DONGAlex03 <lyzerdong03@gmail.com>
2023-12-31 03:07:27 +0100

    test: workflow

--- project/backend/db/create_table.sql
+++ project/backend/db/create_table.sql
@@ -7,30 +7,30 @@ CREATE TABLE books (
     cover VARCHAR(255) NOT NULL
 );
 
-INSERT INTO books(
-       id,
-       title, 
-       `desc`,
-       price,
-       cover) 
-VALUES(
-       2,
-       "The DevOps Handbook: How to Create World-Class Agility, Reliability, & Security in Technology Organizations", 
-       "This award-winning and bestselling business handbook for digital transformation is now fully updated and expa",
-       36,
-       "https://i.ibb.co/KKjtSg2/819-Ow-Bru-Pl-L-SL1500.jpg"
-);
+-- INSERT INTO books(
+--        id,
+--        title, 
+--        `desc`,
+--        price,
+--        cover) 
+-- VALUES(
+--        2,
+--        "The DevOps Handbook: How to Create World-Class Agility, Reliability, & Security in Technology Organizations", 
+--        "This award-winning and bestselling business handbook for digital transformation is now fully updated and expa",
+--        36,
+--        "https://i.ibb.co/KKjtSg2/819-Ow-Bru-Pl-L-SL1500.jpg"
+-- );
 
-INSERT INTO books(
-       id,
-       title, 
-       `desc`,
-       price,
-       cover) 
-VALUES(
-       1,
-       "Hands-on Machine Learning With Scikit-learn, Keras, and Tensorflow: Concepts, Tools, and Techniques to Build Intelligent Systemss", 
-       "Through a recent series of breakthroughs, deep learning has boosted the entire field of machine learning.",
-       77,
-       "https://i.ibb.co/X8m4yzd/81q-HV3-ACap-L-SL1500.jpg"
-);
+-- INSERT INTO books(
+--        id,
+--        title, 
+--        `desc`,
+--        price,
+--        cover) 
+-- VALUES(
+--        1,
+--        "Hands-on Machine Learning With Scikit-learn, Keras, and Tensorflow: Concepts, Tools, and Techniques to Build Intelligent Systemss", 
+--        "Through a recent series of breakthroughs, deep learning has boosted the entire field of machine learning.",
+--        77,
+--        "https://i.ibb.co/X8m4yzd/81q-HV3-ACap-L-SL1500.jpg"
+-- );
